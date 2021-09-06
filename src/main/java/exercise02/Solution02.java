52c149f01ce59647ab43d96197da270365230895
richard lopez <richardlopez813>
2021-09-05 22:01:33 -0400

    final

--- src/main/java/exercise02/Solution02.java
+++ src/main/java/exercise02/Solution02.java
@@ -1,3 +1,15 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Richard Lopez
+ */
+
+/*
+    1. Input Scanner
+    2. Ask for input string
+    3. Scan for string
+    4. Output string with amount of characters
+ */
+
 package exercise02;
 
 import java.util.Scanner;
@@ -6,7 +18,7 @@ public class Solution02 {
 
     private static final Scanner in = new Scanner(System.in);
 
-    public static void main(String args[]){
+    public static void main(String[] args){
         System.out.print("What is the input string? " );
         String string = in.nextLine();
 
